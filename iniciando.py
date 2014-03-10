e7750f5d2dcfb08d2654cd13060f93b596dba6f4
Nicolás Bortolotti <nbortolotti@gmail.com>
2014-03-10 13:11:33 -0600

    Utilizando User Service - simple example
    Utilizando el servicio de usuario para manejar la autenticación e
    información en la solución

--- iniciando.py
+++ iniciando.py
@@ -7,14 +7,20 @@ Created by Nick Bortolotti on 2014-03-07.
 Apache Licence 2.0
 """
 
+from google.appengine.api import users
+
 import webapp2
 
+
 class Principal(webapp2.RequestHandler):
+
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Iniciando el desarrollo')
-
+		usuario = users.get_current_user() #Obtenemos el usuario actual si esta autenticado
+		if usuario:
+			self.response.headers['Content-Type'] = 'text/plain' #Si tenemos un usuario autenticado mostramos un mensaje personalizado
+			self.response.write('Bienvenido, ' + usuario.nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri)) #Si no tenemos usuario autenticado podemos enviarlo con un redirect a la pantalla de autenticacion
 application = webapp2.WSGIApplication([
-	('/',Principal)
-], debug=True)
-
+	('/', Principal)],
+debug=True)
